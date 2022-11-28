adb7481964a6a31cfc21d308be3331ca5687f923
Sofia Martin <sofiapeinetti@gmail.com>
2022-11-27 23:32:10 -0300

    No se puede alquilar con fondos insuf

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -23,24 +23,24 @@ class RentalsController < ApplicationController
     @rental=current_user.rental
     hr=@rental.hours
     h=rental_params[:hours].to_f
-    if current_user.can_rent?(@rental.price)
+    if current_user.can_rent?(rental_params[:hours].to_f * rental_params[:price].to_f)
       if @rental.update rental_params
         update_user_and_car
         @rental.hours= hr+h
         @rental.save
         redirect_to user_path(current_user), notice: '¡Alquiler extendido con exito!'
       else
-        render :edit, notice: 'Ocurrio un error inesperado, por favor intente nuevamente'
+        render :edit, alert: 'Ocurrio un error inesperado, por favor intente nuevamente'
       end
     else
-      redirect_to user_path(current_user), notice:'No cuenta con suficiente dinero para este alquiler. Por favor, ingrese más dinero en la billetera virtual y vuelva a intentarlo'
+      redirect_to user_path(current_user), alert:'No cuenta con suficiente dinero para este alquiler. Por favor, ingrese más dinero en la billetera virtual y vuelva a intentarlo'
     end
   end
 
   def create
     if current_user.travelling?
       flash[:alert] = 'Ya tienes un viaje en curso, finaliza el actual y vuelve a intentarlo'
-      redirect_to user_path(current_user) and return
+      redirect_to rental_path(@rental, car_id: @rental.car) and return
     end
     @rental = Rental.new(user: current_user,
                          car: @car,
@@ -50,7 +50,7 @@ class RentalsController < ApplicationController
       if @rental.valid?
         update_user_and_car
         @rental.save
-        redirect_to car_path(@car), notice: '¡Alquiler realizado con éxito! Tu auto te está esperando!'
+        redirect_to rental_path(@rental, car_id: @rental.car), notice: '¡Alquiler realizado con éxito! ¡Tu auto te está esperando!'
       end
     else
       redirect_to cars_path, alert: 'No cuenta con suficiente dinero para este alquiler. Por favor, ingrese más dinero en la billetera virtual y vuelva a intentarlo'
