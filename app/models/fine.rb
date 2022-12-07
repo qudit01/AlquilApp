85280f36afbecc0e49d162730658519b0140d438
crezdb1425 <cristianriverasd@gmail.com>
2022-12-07 01:30:31 +0000

    generar multa

--- app/models/fine.rb
+++ app/models/fine.rb
@@ -1,3 +1,14 @@
 class Fine < ApplicationRecord
     belongs_to :rental
+
+    enum state: { unpaid: 0, paid: 1 }
+
+
+    def estado
+        if state=='unpaid'
+          "#{'Pendiente de pago'}"
+        else
+          "#{'Pagada'}"
+        end
+    end
 end
